540941808c5072fceb103615727aa72d49ce1a93
jadehildebrand <73186380+jadehildebrand@users.noreply.github.com>
2022-02-23 12:39:03 -0500

    Switch
    initialize two switches, create methods to grab their status

--- src/main/java/frc/robot/subsystems/ArmSS.java
+++ src/main/java/frc/robot/subsystems/ArmSS.java
@@ -8,13 +8,19 @@ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.TalonSRXSimCollection;
 import com.ctre.phoenix.motorcontrol.GroupMotorControllers;
 
-
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 
 public class ArmSS extends SubsystemBase{
     private TalonSRX talon0;
     private TalonSRX talon1;
+    
+    DigitalInput limitSwitchUP = new DigitalInput(6);   //change number
+    Counter counterUP = new Counter(limitSwitchUP);
 
+    DigitalInput limitSwitchDown = new DigitalInput(7);     //change number
+    Counter counterDown = new Counter(limitSwitchDown);
 
 public ArmSS() {
     talon0 = new TalonSRX(30); 
@@ -34,4 +40,21 @@ public void armdown(){
 public void stop() {
     talon0.set(ControlMode.PercentOutput, 0.0);
   }
+  
+    
+public boolean UPSwitchSet(){       //Upper switch returns a 1 when triggered and a 0 when not
+      return counterUP.get() > 0;
+  }
+
+public boolean DownSwitchSet(){       //Lower switch returns a 1 when triggered and a 0 when not
+    return counterDown.get() > 0;
+}
+
+public void initalizeCounterUP(){     //reset the Upper counter
+    counterUP.reset();
+}
+
+public void initalizeCounterDown(){     //reset the Lower counter
+    counterUP.reset();
+}
 }
