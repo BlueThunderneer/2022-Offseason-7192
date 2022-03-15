675b7c99661d77ad674d3256289a0e829467e489
BlueThunderneer <81537440+BlueThunderneer@users.noreply.github.com>
2022-03-14 21:01:58 -0400

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -41,7 +41,7 @@ public class Robot extends TimedRobot {
     public void robotInit() {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard
-        m_ArmSS.initialize();
+        // m_ArmSS.initialize();
         m_robotContainer = RobotContainer.getInstance();
         HAL.report(tResourceType.kResourceType_Framework, tInstances.kFramework_RobotBuilder);
     }
